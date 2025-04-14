260f11ae064075e5f0d30d16c238882e24dfaafd
AnnaO <annaoleks88@gmail.com>
2025-04-14 17:43:20 +0300

    .

--- tabelisisu.php
+++ tabelisisu.php
@@ -2,50 +2,110 @@
 //votame uhendust conf.php failist
 require("conf.php");
 
+//tabeli sisu naitamine
+//select id, pealkiri, sisu from lehed
 global $yhendus;
-//kustutamine - peab olema koodis esimesena
-if(isset($_REQUEST['kustuta'])){
+//kustutamine peab olema koodis esimesena
+if (isset($_REQUEST['kustuta'])){
     $kask=$yhendus->prepare('DELETE FROM lehed WHERE id=?');
     $kask->bind_param('i', $_REQUEST['kustuta']);
     //i-integer, s-string
     $kask->execute();
 }
-//tabeli sisu naitamine
-//SELECT id, pelkiri, sisu FROM lehed
+
+if(isSet($_REQUEST["uusleht"])){
+    $kask=$yhendus->prepare("INSERT INTO lehed (pealkiri, sisu, kuupaev) VALUES (?, ?, ?)");
+    $kask->bind_param("sss", $_REQUEST["pealkiri"], $_REQUEST["sisu"], $_REQUEST["kuupaev"]);
+    $kask->execute();
+    header("Location: $_SERVER[PHP_SELF]");
+    $yhendus->close();
+    exit();
+}
+
 $kask=$yhendus->prepare('SELECT id, pealkiri, sisu, kuupaev FROM lehed');
 $kask->bind_result($id, $pealkiri, $sisu, $kuupaev);
 $kask->execute();
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Tabeli sisu naitamine</title>
+    <title>Tabeli sisu näitamine</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-<h1>Tabeli "lehed" sisu</h1>
-<table>
-    <tr>
-        <th>id</th>
-        <th>pealkiri</th>
-        <th>sisu</th>
-        <th>kuupaev</th>
-        <th>kustuta</th>
-        <th>dfv</th>
-    </tr>
-    <?php
-    //read tabelist tsukliga
-    while ($kask->fetch()) {
-        echo "<tr>";
-        echo "<td>".htmlspecialchars($id)."</td>";
-        echo "<td>".htmlspecialchars($pealkiri)."</td>";
-        echo "<td>".htmlspecialchars($sisu)."</td>";
-        echo "<td>".htmlspecialchars($kuupaev)."</td>";
-        echo "<td><a href='?kustuta=$id'>X</a></td>";
-        echo "</tr>";
-        echo "<br>";
-    }
-    ?>
-</table>
+<main>
+    <br>
+    <h1>Tabeli "lehed" sisu</h1>
+    <br>
+    <table id="menyykiht">
+        <tr>
+            <th>id</th>
+            <th>pealkiri</th>
+            <th>sisu</th>
+            <th>kuupäev</th>
+            <th>kustuta</th>
+        </tr>
+
+        <?php
+        //read tabelist tsukliga
+        while ($kask->fetch()) {
+            echo "<tr>";
+            echo "<td>".htmlspecialchars($id)."</td>";
+            echo "<td>".htmlspecialchars($pealkiri)."</td>";
+            echo "<td>".htmlspecialchars($sisu)."</td>";
+            echo "<td>".htmlspecialchars($kuupaev)."</td>";
+            echo "<td><a href='?kustuta=$id'><strong>X</strong></a></td>";
+            echo "</tr>";
+        }
+        ?>
+    </table>
+    <br>
+    <br>
+    <a href='<?="$_SERVER[PHP_SELF]?lisamine=jah" ?>'>Lisa</a>
+
+    <div id="sisukiht">
+        <?php
+        if(isSet($_REQUEST["id"])){
+            $kask=$yhendus->prepare("SELECT id, pealkiri, sisu, kuupaev FROM lehed WHERE id=?");
+            $kask->bind_param("i", $_REQUEST["id"]);
+            $kask->bind_result($id, $pealkiri, $sisu, $kuupaev);
+            $kask->execute();
+            if($kask->fetch()){
+                echo "<h2>".htmlspecialchars($pealkiri)."</h2>";
+                echo htmlspecialchars($sisu);
+                echo "<h2>".htmlspecialchars($kuupaev)."</h2>";
+                echo "<br /><a href='$_SERVER[PHP_SELF]?kustutusid=$id'>kustuta</a>";
+            } else {
+                echo "Vigased andmed.";
+            }
+        }
+        if(isSet($_REQUEST["lisamine"])){
+            ?>
+            <form action='<?=$_SERVER["PHP_SELF"] ?>' id="forma">
+                <input type="hidden" name="uusleht" value="jah" />
+                <h2>Uue teate lisamine</h2>
+                <dl>
+                    <dt>Pealkiri:</dt>
+                    <dd>
+                        <input type="text" name="pealkiri"/>
+                    </dd>
+                    <dt>Teate sisu:</dt>
+                    <dd>
+                        <textarea rows="20" name="sisu"></textarea>
+                    </dd>
+                    <dt>Kuupäev:</dt>
+                    <dd>
+                        <input type="date" name="kuupaev"/>
+                    </dd>
+                </dl>
+                <input type="submit" value="sisesta">
+            </form>
+            <?php
+        }
+        ?>
+    </div>
+</main>
 </body>
 </html>
--- teadevalik.php
+++ teadevalik.php
@@ -1,12 +1,32 @@
 <?php
 require("conf.php");
 global $yhendus;
+if(isSet($_REQUEST["uusleht"])){
+    $kask=$yhendus->prepare("INSERT INTO lehed (pealkiri, sisu) VALUES (?, ?)");
+    $kask->bind_param("ss", $_REQUEST["pealkiri"], $_REQUEST["sisu"]);
+    $kask->execute();
+    header("Location: $_SERVER[PHP_SELF]");
+    $yhendus->close();
+    exit();
+}
+if(isSet($_REQUEST["kustutusid"])){
+    $kask=$yhendus->prepare("DELETE FROM lehed WHERE id=?");
+    $kask->bind_param("i", $_REQUEST["kustutusid"]);
+    $kask->execute();
+}
+if(isSet($_REQUEST["muutmisid"])){
+    $kask=$yhendus->prepare("UPDATE lehed SET pealkiri=?, sisu=? WHERE id=?");
+    $kask->bind_param("ssi", $_REQUEST["pealkiri"], $_REQUEST["sisu"],
+        $_REQUEST["muutmisid"]);
+    $kask->execute();
+}
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html>
+        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
     <title>Teated lehel</title>
+    <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
     <style type="text/css">
         #menyykiht{
             float: left;
@@ -29,30 +49,68 @@ global $yhendus;
         $kask->bind_result($id, $pealkiri);
         $kask->execute();
         while($kask->fetch()){
-            echo "<li><a href='teadetevalik.php?id=$id'>".
-                htmlspecialchars($pealkiri)."</a></li>";
+            echo "<li><a href='$_SERVER[PHP_SELF]?id=$id'>".htmlspecialchars($pealkiri)."</a></li>";
         }
         ?>
     </ul>
+    <a href='<?="$_SERVER[PHP_SELF]?lisamine=jah" ?>'>Lisa ...</a>
 </div>
 <div id="sisukiht">
     <?php
     if(isSet($_REQUEST["id"])){
-        $kask=$yhendus->prepare("SELECT id, pealkiri, sisu FROM lehed
-WHERE id=?");
-//Kysim2rgi asemele pannakse aadressiribalt tulnud id,
-//eeldatakse, et ta on tyybist integer (i). (double - d, string - s)
+        $kask=$yhendus->prepare("SELECT id, pealkiri, sisu FROM lehed WHERE id=?");
         $kask->bind_param("i", $_REQUEST["id"]);
         $kask->bind_result($id, $pealkiri, $sisu);
         $kask->execute();
         if($kask->fetch()){
-            echo "<h2>".htmlspecialchars($pealkiri)."</h2>";
-            echo htmlspecialchars($sisu);
+            if(isSet($_REQUEST["muutmine"])){
+                echo "
+<form action='$_SERVER[PHP_SELF]'>
+<input type='hidden' name='muutmisid' value='$id'/>
+<h2>Teate muutmine</h2>
+<dl>
+<dt>Pealkiri:</dt>
+<dd>
+<input type='text' name='pealkiri' value='".
+                    htmlspecialchars($pealkiri)."'/>
+ </dd>
+ <dt>Teate sisu:</dt>
+ <dd>
+ <textarea rows='20' cols='30' name='sisu'>".
+                    htmlspecialchars($sisu)."</textarea>
+ </dd>
+ </dl>
+ <input type='submit' value='Muuda' />
+ </form>
+ ";
+            } else {
+                echo "<h2>".htmlspecialchars($pealkiri)."</h2>";
+                echo htmlspecialchars($sisu);
+                echo "<br /><a href='$_SERVER[PHP_SELF]?kustutusid=$id'>kustuta</a> ";
+                echo "<a href='$_SERVER[PHP_SELF]?id=$id&amp;muutmine=jah'>muuda</a>";
+            }
         } else {
             echo "Vigased andmed.";
         }
-    } else {
-        echo "Tere tulemast avalehele! Vali men&uuml;&uuml;st sobiv teema.";
+    }
+    if(isSet($_REQUEST["lisamine"])){
+        ?>
+        <form action='<?=$_SERVER["PHP_SELF"] ?>'>
+            <input type="hidden" name="uusleht" value="jah" />
+            <h2>Uue teate lisamine</h2>
+            <dl>
+                <dt>Pealkiri:</dt>
+                <dd>
+                    <input type="text" name="pealkiri" />
+                </dd>
+                <dt>Teate sisu:</dt>
+                <dd>
+                    <textarea rows="20" cols="30" name="sisu"></textarea>
+                </dd>
+            </dl>
+            <input type="submit" value="sisesta" />
+        </form>
+        <?php
     }
     ?>
 </div>
@@ -64,3 +122,4 @@ WHERE id=?");
 <?php
 $yhendus->close();
 ?>
+
